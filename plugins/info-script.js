01c8208779d20a1434ba5271858a56a1de08f519
AzzBott679 <107923326+AzzBott679@users.noreply.github.com>
2022-07-16 22:35:56 +0700

    Add files via upload

---
+++ plugins/info-script.js
@@ -0,0 +1,15 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+let info = `Bot ini menggunakan script
+
+Recode By AzzBot-MD`
+
+let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+conn.reply(m.chat, info, m, { contextInfo: { externalAdReply: {title: global.nameown, body: `Jangan Lupa Follow Yak Om`, sourceUrl: snh, thumbnail: fs.readFileSync('./thumbnail.jpg')  }}})
+}
+handler.help = ['sc']
+handler.tags = ['info']
+handler.command = /^(s(ourcode|c))$/i
+
+export default handler
